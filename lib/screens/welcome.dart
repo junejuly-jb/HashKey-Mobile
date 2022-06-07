c51dbaaeb49f5aab303e83c9a639c3f94995abd2
june <junearagoncillo@gmail.com>
2022-06-07 14:41:17 +0800

    added provider and validations

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/connection_provider.dart';
 
 
 class WelcomeScreen extends StatelessWidget {
