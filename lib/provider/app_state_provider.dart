58b9a3315ad0db7445622fa38421f63490919f17
june <junearagoncillo@gmail.com>
2022-06-29 13:58:59 +0800

    added generate screen

--- lib/provider/app_state_provider.dart
+++ lib/provider/app_state_provider.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 class AppStateProvider with ChangeNotifier{
   String _appState = 'home';
+  bool numberState = false;
+  bool letterState = false;
+  bool sysmbolState = false;
+
   String get appState => _appState;
 
   void setAppState(String val){
