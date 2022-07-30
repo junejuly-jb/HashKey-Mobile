ac0c45b879a72725882fe1c5bf8efe57a122f0c1
JBA <junearagoncillo@gmail.com>
2022-07-31 00:17:52 +0800

    added new heart tab

---
+++ lib/shared/tabview/credential_healthstat.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CredentialHealthStatus extends StatelessWidget {
+  const CredentialHealthStatus({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 50.h,),
+          Text('Test'),
+        ],
+      ),
+    );
+  }
+}
